18d7989886cc850b29ca234edd6f1a8920180af7
enriqueseor <enriqueseor@gmail.com>
2023-07-24 17:54:12 +0100

    mainActivity enhancement

--- app/src/main/java/net/atos/pokedex/ui/pokemon/MainActivity.kt
+++ app/src/main/java/net/atos/pokedex/ui/pokemon/MainActivity.kt
@@ -1,9 +1,11 @@
 package net.atos.pokedex.ui.pokemon
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import net.atos.pokedex.R
 import net.atos.pokedex.data.model.PokemonModel
@@ -18,17 +20,23 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private var retrofit: Retrofit? = null
-    private var pokemonAdapterList: PokemonAdapter? = null
+    private var pokemonAdapter: PokemonAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //RECYCLER VIEW
         val recyclerView = findViewById<View>(R.id.recyclerViewPokemon) as RecyclerView
-        pokemonAdapterList = PokemonAdapter(this.baseContext)
-        recyclerView.adapter = pokemonAdapterList
+        pokemonAdapter = PokemonAdapter(this.baseContext)
+        recyclerView.adapter = pokemonAdapter
         recyclerView.setHasFixedSize(true)
-        val gridLayoutManager = GridLayoutManager(this, 1)
-        recyclerView.layoutManager = gridLayoutManager
+
+        //LINEAR LAYOUT
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
+        //RETROFIT
         retrofit = Retrofit.Builder()
             .baseUrl("https://pokeapi.co/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -38,23 +46,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun getData() {
         val service = retrofit!!.create(PokemonAPIService::class.java)
-        val pokemonFetchResultsCall = service.pokemons
-        pokemonFetchResultsCall?.enqueue(object : Callback<PokemonResponse?> {
-            override fun onResponse(
-                call: Call<PokemonResponse?>,
-                response: Response<PokemonResponse?>
-            ) {
+        val pokemonResponse = service.pokemons
+        pokemonResponse?.enqueue(object : Callback<PokemonResponse?> {
+            override fun onResponse(call: Call<PokemonResponse?>, response: Response<PokemonResponse?>) {
                 if (response.isSuccessful) {
-                    val pokemonFetchResults = response.body()!!
-                    val pokemonList = pokemonFetchResults.results
-                    for (i in pokemonList?.indices!!) {
-                        pokemonList[i]
+                    val responseBody = response.body()!!
+                    responseBody.let {
+                        val pokemonList = it.results
+                        pokemonAdapter?.addPokemon(pokemonList as ArrayList<PokemonModel>?)
                     }
-                    pokemonAdapterList!!.addPokemon(pokemonList as ArrayList<PokemonModel>?)
                 }
             }
             override fun onFailure(call: Call<PokemonResponse?>, t: Throwable) {
-                TODO("Not yet implemented")
+                Log.e("MainActivity", "Network request failed: ${t.message}")
+                Toast.makeText(this@MainActivity, "Network request failed.", Toast.LENGTH_SHORT).show()
             }
         })
     }
