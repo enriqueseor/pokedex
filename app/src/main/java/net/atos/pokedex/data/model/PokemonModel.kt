f913535e4e625b39e25dbd9c174a61394afce598
enriqueseor <enriqueseor@gmail.com>
2023-10-01 14:50:08 +0100

    Pokemon model class unnecesary parameter

--- app/src/main/java/net/atos/pokedex/data/model/PokemonModel.kt
+++ app/src/main/java/net/atos/pokedex/data/model/PokemonModel.kt
@@ -1,10 +1,3 @@
 package net.atos.pokedex.data.model
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-class PokemonModel {
-    @SerializedName("name")
-    @Expose
-    var name: String? = null
-}
+class PokemonModel
